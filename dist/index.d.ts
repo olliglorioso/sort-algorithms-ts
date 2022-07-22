4d1735f73a8d982b1fe2387f18b278b1c44ab28b
Olli Glorioso <84347872+olliglorioso@users.noreply.github.com>
2022-07-22 22:31:45 +0300

    Gitignore and delete dist

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { compare, randomSample } from "./extras";
-export { insertionSort, mergeSort, quickSort } from "./sorts";
-export { ascendingCompare, descendingCompare, randomNumArray, shuffle } from "./util";
--- dist/sorts.d.ts
+++
@@ -1,4 +0,0 @@
-import { QuickSortPropsT, SortPropsT } from "./types";
-export declare const insertionSort: ({ arr, compare }: SortPropsT) => Array<number>;
-export declare const mergeSort: ({ arr, compare }: SortPropsT) => Array<number>;
-export declare const quickSort: ({ arr, pivotLocation, compare }: QuickSortPropsT) => Array<number>;
